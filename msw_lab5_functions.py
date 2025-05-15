b5ccd4c0fe6ffe4f62eb0a0a0036e5fad3c31dde
maxwhitehouse <146250785+maxwhitehouse@users.noreply.github.com>
2025-05-15 16:21:32 -0700

    Add files via upload

---
+++ msw_lab5_functions.py
@@ -0,0 +1,51 @@
+#####################
+# Block 1:  Import the packages you'll need
+# 
+# 
+
+import os, sys
+import rasterio
+import geopandas as gpd
+
+
+
+
+##################
+# Block 2: 
+# set the working directory to the directory where the data are
+
+# Change this to the directory where your data are
+
+data_dir = r"R:\2025\Spring\GEOG562\Students\whitehom\Lab5"
+os.chdir(data_dir)
+print(os.getcwd())
+
+
+##################
+# Block 3: 
+#   Set up a new smart raster class using rasterio  
+#    that will have a method called "calculate_ndvi"
+
+
+
+
+
+
+
+
+
+
+
+##################
+# Block 4: 
+#   Set up a new smart vector class using geopandas
+#    that will have a method similar to what did in lab 4
+#    to calculate the zonal statistics for a raster
+#    and add them as a column to the attribute table of the vector
+
+
+
+
+
+
+
---
+++ mw_lab5_scripts.py
@@ -0,0 +1,51 @@
+# Lab 5 scripts
+
+
+import msw_lab5_functions as l5
+
+#  Part 1:
+
+#  Assign a variable to the Landsat file 
+
+
+# Pass this to your new smart raster class
+
+
+
+# Calculate NDVI and save to and output file
+
+
+
+
+
+
+# Part 2:
+# Assign a variable to the parcels data shapefile path
+
+
+#  Pass this to your new smart vector class
+
+
+#  Calculate zonal statistics and add to the attribute table of the parcels shapefile
+
+
+
+#  Part 3: Optional
+#  Use matplotlib to make a map of your census tracts with the average NDVI values
+
+
+
+
+
+
+
+
+# Simple Python script
+
+# Print a greeting
+print("Hello, world!")
+
+# Calculate the sum of the first 10 positive integers
+total = sum(range(1, 11))
+print("The sum of the first 10 positive integers is:", total)
+
